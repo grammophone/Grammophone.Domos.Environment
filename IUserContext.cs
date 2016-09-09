eb6897969284c01bcdc8d3b5aad519ff5f96150e
grammophone <emmanuel.liossis@gmail.com>
2016-09-09 15:07:38 +0300

    Initial commit.

---
+++ IUserContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Domos.Environment
+{
+	/// <summary>
+	/// Bridges the authentication environment to the 
+	/// business layer.
+	/// </summary>
+	public interface IUserContext
+	{
+		/// <summary>
+		/// The ID of the authenticated, registered user,
+		/// else null if the user is anonymous.
+		/// </summary>
+		long? UserID { get; }
+	}
+}
